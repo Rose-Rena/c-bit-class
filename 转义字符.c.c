32bd90444801bd1f879f74cab60c071085df47e9
Rose-Rena <127564677+Rose-Rena@users.noreply.github.com>
2023-03-14 18:26:53 -0700

    Add files via upload

---
+++ 转义字符.c.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//转义字符：转变了原来的意思！！
+#include <stdio.h>
+
+int main()
+{
+    // printf("c:\test\test.c");
+    // printf("ab\ncd")
+    //\' - printf("%c\n", '\'');
+    //printf("%s\n"), "\"");
+    // \ddd - ddd表示1-3个八进制（0-7）的数字。如\130 X
+    // \xdd - dd表示2个十六进制数字. 如：\x30 0
+    return 0;
+}
